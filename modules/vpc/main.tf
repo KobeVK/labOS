b900ced1919ea81ef7d1016b69e0a10207a1bdbf
Ubuntu <ubuntu@ip-172-31-19-187.eu-west-3.compute.internal>
2023-04-21 12:18:49 +0000

    chore: trying new infra

---
+++ modules/vpc/main.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+
+    name = var.vpc_name
+    cidr = var.vpc_cidr
+
+    azs = var.vpc_azs
+    private_subnets = var.vpc_private_subnets
+    public_subnets = var.vpc_public_subnets
+
+    enable_nat_gateway = var.vpc_enable_nat_gateway
+
+    tags = var.vpc_tags
+}
